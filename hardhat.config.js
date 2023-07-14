1834a8b0c8bfecacf3fd3c887d7934dc285a145d
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-14 23:54:05 +0300

    added images

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-  hardhat: {
-    chainId: 31337,
-    // gas: 2100000,
-    // gasPrice: 8000000000,
+  defaultNetwork: "localhost",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.ENDPOINT_URL,
+      accounts: [process.env.DEPLOYER_KEY],
+    },
   },
-  // networks: {
-  //   hardhat: {},
-  //   polygon_mumbai: {
-  //     url: "Your alchemy url",
-  //     accounts: [`0x${"Your private key here"}`],
-  //   },
-  // },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }, 
+  paths: {
+    sources: "./contracts",
+    artifacts: "./Context"
+  },
+  mocha: {
+    timeout: 4000
+  }
 };
